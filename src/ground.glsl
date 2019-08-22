19ebaa0f9aabb90f28d58092129a9b38810d7015
Hamish Milne <hamishmilne83@gmail.com>
2019-08-22 21:05:04 +0100

    Shaders now work on GLSL

--- src/ground.glsl
+++ src/ground.glsl
@@ -2,26 +2,26 @@ uniform Image map;
 uniform Image atlas;
 uniform vec2 offset;
 
-const int tile_width = 32;
-const int tile_height = 16;
-const int atlas_stride = 16;
+const float tile_width = 32.0;
+const float tile_height = 16.0;
+const float atlas_stride = 16.0;
 
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
 {
     screen_coords += offset;
     vec2 uv = vec2(
-        int((2*screen_coords.y + screen_coords.x) / 32),
-        int((2*screen_coords.y - screen_coords.x) / 32)
+        floor((2.0*screen_coords.y + screen_coords.x) / 32.0),
+        floor((2.0*screen_coords.y - screen_coords.x) / 32.0)
     );
     vec2 tile_origin = vec2(
         (uv.x - uv.y) * tile_width,
         (uv.x + uv.y) * tile_height
-    ) / 2;
-    vec2 tile_uv = (screen_coords - tile_origin) + vec2(tile_width/2, 0);
-    int tile_idx = int(ceil(Texel(map, uv/128).b)); // TODO: Do the index properly here
+    ) / 2.0;
+    vec2 tile_uv = (screen_coords - tile_origin) + vec2(tile_width/2.0, 0);
+    float tile_idx = ceil(Texel(map, uv/128.0).b); // TODO: Do the index properly here
     vec2 tile_pixels = vec2(
-        mod(tile_idx, atlas_stride) * tile_width,
-        int(tile_idx / atlas_stride) * tile_height
+        floor(mod(tile_idx, atlas_stride)) * tile_width,
+        floor(tile_idx / atlas_stride) * tile_height
     ) + tile_uv;
     vec2 scale = vec2(tile_width, tile_height) * atlas_stride;
 
--- src/outline.glsl
+++ src/outline.glsl
@@ -9,7 +9,7 @@ float check(Image tex, vec2 coords, vec2 dir)
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
 {
     vec4 texturecolor = Texel(tex, texture_coords);
-    if (texturecolor.a > 0) {
+    if (texturecolor.a > 0.0) {
         return texturecolor * color;
     }
     float surround_alpha =
@@ -21,7 +21,7 @@ vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
         check(tex, texture_coords, vec2(-1, -1)) +
         check(tex, texture_coords, vec2(-1,  1)) +
         check(tex, texture_coords, vec2( 1, -1));
-    if (surround_alpha > 0) {
+    if (surround_alpha > 0.0) {
         return outline_color;
     }
     return vec4(0, 0, 0, 0);
